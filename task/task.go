1731c117f142a22faf82c0a6d37876ec2e8df1ce
fghwett <1058178245@qq.com>
2021-11-09 11:48:07 +0800

    去除取消的朴朴分享任务

--- task/task.go
+++ task/task.go
@@ -42,19 +42,19 @@ func (t *Task) Do() {
 		return
 	}
 
-	util.SmallSleep(1000, 3000)
-
-	if err := t.shareTask(); err != nil {
-		t.result = append(t.result, fmt.Sprintf("【分享任务】：失败 %s", err))
-		return
-	}
-
-	//util.SmallSleep(800, 2000)
+	//util.SmallSleep(1000, 3000)
 	//
-	//if err := t.queryPointTask(); err != nil {
-	//	t.result = append(t.result, fmt.Sprintf("【查询积分任务】：失败 %s", err))
+	//if err := t.shareTask(); err != nil {
+	//	t.result = append(t.result, fmt.Sprintf("【分享任务】：失败 %s", err))
 	//	return
 	//}
+
+	util.SmallSleep(800, 2000)
+
+	if err := t.queryPointTask(); err != nil {
+		t.result = append(t.result, fmt.Sprintf("【查询积分任务】：失败 %s", err))
+		return
+	}
 
 	util.SmallSleep(800, 2000)
 
